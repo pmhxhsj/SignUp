d5327d878ee3077d5e7ed17025e971cefdf8c809
pmhxhsj <minho7952@naver.com>
2022-01-07 14:42:54 +0900

    feat: 토큰 생성 및 인증

--- models/User.js
+++ models/User.js
@@ -1,10 +1,14 @@
 const { builtinModules } = require('module');
 const mongoose = require('mongoose');
+const { runInNewContext } = require('vm');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const saltRounds = 10;
 
 const userSchema = mongoose.Schema({
   name: {
     type: String,
-    maxlength: 20,
+    maxlength: 50,
   },
   email: {
     type: String,
@@ -13,7 +17,7 @@ const userSchema = mongoose.Schema({
   },
   password: {
     type: String,
-    maxlength: 20,
+    maxlength: 500,
   },
   role: {
     type: Number,
@@ -27,6 +31,55 @@ const userSchema = mongoose.Schema({
   },
 });
 
+userSchema.pre('save', function (next) {
+  var user = this;
+
+  //비밀번호 암호화
+  if (user.isModified('password')) {
+    bcrypt.genSalt(saltRounds, function (err, salt) {
+      if (err) return next(err);
+
+      bcrypt.hash(user.password, salt, function (err, hash) {
+        if (err) return next(err);
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
+    next();
+  }
+});
+
+userSchema.methods.comparePassword = function (plainPassword, cb) {
+  bcrypt.compare(plainPassword, this.password, function (err, isMatch) {
+    if (err) return cb(err);
+    cb(null, isMatch);
+  });
+};
+
+userSchema.methods.generateToken = function (cb) {
+  var user = this;
+  var token = jwt.sign(user._id.toHexString(), 'secretToken');
+
+  user.token = token;
+  user.save(function (err, user) {
+    if (err) return cb(err);
+    cb(null, user);
+  });
+};
+
+userSchema.statics.findByToken = function (token, cb) {
+  var user = this;
+  // 토큰을 decode
+  jwt.verify(token, 'secretToken', function (err, decoded) {
+    // 유저 아이디를 이용해서 유저를 찾은 다음
+    // 클라이언트에서 가져온 토큰과 db에 보관된 토큰이 있는지 확인
+    user.findOne({ _id: decoded, token: token }, function (err, user) {
+      if (err) return cb(err);
+      cb(null, user);
+    });
+  });
+};
 const User = mongoose.model('User', userSchema);
 
 module.exports = { User };
