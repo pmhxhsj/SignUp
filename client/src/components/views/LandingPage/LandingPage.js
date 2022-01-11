06e86ccd98ea250a10860b65a7c39f2570a0075d
pmhxhsj <minho7952@naver.com>
2022-01-11 18:33:35 +0900

    feat: 로그인, 로그아웃 헤더 기능 교체

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,14 +1,18 @@
 import React, { useEffect } from 'react';
+import { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import styles from '../../../style/Style.module.css';
 import btnStyles from '../../../style/Btn.module.css';
 import { Link } from 'react-router-dom';
 import Footer from '../Footer/Footer';
+import { useDispatch } from 'react-redux';
+import { auth } from '../../../_actions/user_action';
+import { set } from 'mongoose';
 function LandingPage() {
-  useEffect(() => {
-    axios.get('/api/hello').then((res) => console.log(res));
-  }, []);
+  // useEffect(() => {
+  //   axios.get('/api/hello').then((res) => console.log(res));
+  // }, []);
 
   let navigate = useNavigate();
 
@@ -17,40 +21,72 @@ function LandingPage() {
       if (response.data.success) {
         navigate('/login');
       } else {
-        alert('안됨');
+        alert('로그인되지 않았습니다.');
       }
     });
   };
+
+  function BoolLogin() {
+    const [isLogin, setisLogin] = useState(false);
+    const dispatch = useDispatch();
+
+    dispatch(auth()).then((response) => {
+      console.log(response);
+      if (response.payload.isAuth) {
+        console.log('로그인 상태');
+        setisLogin(true);
+      } else {
+        console.log('로그아웃 상태');
+        setisLogin(false);
+      }
+    });
+
+    return isLogin;
+  }
   return (
     <div>
       <header className={styles.header}>
         <div className={styles.firstHeader}>
-          <Link to='/login'>
-            <button className={btnStyles.landingPageBtn}>로그인</button>
-          </Link>
-
-          <Link to='/login'>
-            <button className={btnStyles.landingPageBtn}>마이페이지</button>
-          </Link>
+          {BoolLogin() ? (
+            <div>
+              <button className={btnStyles.landingPageBtn} onClick={onClickHandler}>
+                로그아웃
+              </button>
+              <Link to='/login'>
+                <button className={btnStyles.landingPageBtn}>마이페이지</button>
+              </Link>
+            </div>
+          ) : (
+            <div>
+              <Link to='/login'>
+                <button className={btnStyles.landingPageBtn}>로그인</button>
+              </Link>
+              <Link to='/register'>
+                <button className={btnStyles.landingPageBtn}>회원가입</button>
+              </Link>
+            </div>
+          )}
         </div>
         <h1 className={styles.logo}>Draw Space</h1>
-        <div className={styles.menuBar}>
+        <nav className={styles.menuBar}>
           <button className={btnStyles.landingPageBtn}>Preview</button>
-          <button className={btnStyles.landingPageBtn}>Upload</button>
+          <button className={btnStyles.landingPageBtn}>Photographer</button>
+          <button className={btnStyles.landingPageBtn}>Author</button>
+          <button className={btnStyles.landingPageBtn}>Poet</button>
+          <button className={btnStyles.landingPageBtn}>Artist</button>
           <button className={btnStyles.landingPageBtn}>Review</button>
-        </div>
+        </nav>
 
         <div className={styles.landingImage}>
           <img className={styles.mainImage} src='img/artist.jpg' alt='artist' />
         </div>
       </header>
       <div className={styles.landingPage}>
-        {/* <div className={styles.landingElement}>
-          <button onClick={onClickHandler}>Logout</button>
-        </div> */}
+        <div className={styles.landingElement}>{/* <button onClick={onClickHandler}>Logout</button> */}</div>
       </div>
-
-      <Footer />
+      <footer>
+        <Footer />
+      </footer>
     </div>
   );
 }
